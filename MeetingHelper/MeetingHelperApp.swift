8962700a6ca2c61ced01f111dbc42f59158b4f16
kaisenye <kaisenye16@gmail.com>
2025-06-13 21:03:02 -0700

    Initial Commit

---
+++ MeetingHelper/MeetingHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingHelperApp.swift
+//  MeetingHelper
+//
+//  Created by Kaisen Ye on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetingHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
